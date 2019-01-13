f515d48a8e954bc5601e4b56342f9cec03aea5e5
Mohammad Qabbani <qabbani454@gmail.com>
2019-01-13 15:03:27 +0200

    Create Method`s in Student_Mesthod Class

--- includes/student_method.php
+++ includes/student_method.php
@@ -25,14 +25,51 @@ class student_method extends connection {
             $studentSet[] = $value;
         }
     }
+    
+    public function fetchById($id){
+        $id   = $this->std_id ; 
+        $sql  = "SELECT * FROM `student` WHERE `std_id` = $id ";
+        $stmt = $this->connect->query($sql);
+        $studentSet = array();
+        foreach ($stmt as $value) {
+            $studentSet[] = $value;
+        }
+      
+        
+    }
 
     public function insertStudent() {
+        
         $sql = "INSERT INTO `student`(`std_name`, `std_mobile`, `std_email`, `std_photo`, "
                 . "`std_id_copy`, `std_address`, `std_age`, `std_gender`, `std_dob`, `std_ref_mobile`, "
                 . "`notes`) VALUES ('$this->std_name',$this->std_mobile,'$this->std_email','$this->std_photo',"
                 . "'$this->std_id_copy','$this->std_address',"
                 . "$this->std_age,'$this->std_gender','$this->std_dob',$this->std_ref_mobile,'$this->std_notes')";
-        
+        $stmt = $this->connect->query($sql);
+    }
+
+    public function deleteStudent($id) {
+        $id = $this->std_id;
+        $sql = "DELETE FROM `student` WHERE `std_id` = $id";
+        $stmt = $this->connect->query($sql);
+    }
+
+    public function updateStudent($id) {
+        $id = $this->std_id;
+        if (!empty($this->std_photo) && !empty($this->std_id_copy)) {
+            $sql = "UPDATE `student` SET `std_name`='$this->std_name',`std_mobile`=$this->std_mobile,"
+                    . "`std_email`='$this->std_email',`std_photo`='$this->std_photo',`std_id_copy`='$this->std_id_copy',"
+                    . "`std_address`='$this->std_address',"
+                    . "`std_age`=$this->std_age,`std_gender`='$this->std_gender',`std_dob`=$this->std_dob,"
+                    . "`std_ref_mobile`=$this->std_ref_mobile,"
+                    . "`notes`='$this->std_notes' WHERE `std_id` = $id";
+        } else if (!empty($this->std_photo)) {
+            
+        } else if (!empty($this->std_id_copy)) {
+            
+        } else if (empty($this->std_photo) && empty($this->std_id_copy)) {
+            
+        }
     }
 
 }
--- student.php
+++ student.php
@@ -1,23 +1,51 @@
 <?php
 include './includes/student_method.php';
-if(isset($_POST['fetchAll'])){
+if (isset($_POST['fetchAll'])) {
     $studenClass = new student_method();
     $studenClass->fetchAll();
 }
-if(isset($_POST['add'])){
+if (isset($_POST['add'])) {
     $studenClass = new student_method();
-    $studenClass->std_name         = $_POST['name'];
-    $studenClass->std_mobile       = $_POST['mobile'];
-    $studenClass->std_email        = $_POST['email'];
-    $studenClass->std_photo        = $_POST['photo'];
-    $studenClass->std_id_copy      = $_POST['idcopy'];
-    $studenClass->std_address      = $_POST['address'];
-    $studenClass->std_age          = $_POST['age'];
-    $studenClass->std_gender       = $_POST['gender'];
-    $studenClass->std_dob          = $_POST['dob'];
-    $studenClass->std_ref_mobile   = $_POST['red'];
-    $studenClass->std_notes        = $_POST['notes'];
+    $studenClass->std_name = $_POST['name'];
+    $studenClass->std_mobile = $_POST['mobile'];
+    $studenClass->std_email = $_POST['email'];
+    $studenClass->std_address = $_POST['address'];
+    $studenClass->std_age = $_POST['age'];
+    $studenClass->std_gender = $_POST['gender'];
+    $studenClass->std_dob = $_POST['dob'];
+    $studenClass->std_ref_mobile = $_POST['ref'];
+    $studenClass->std_notes = $_POST['notes'];
+    if ($_FILES['photo']['error'] == 0 && $_FILES['idcopy']['error'] == 0) {
+        // Student Images
+        $image_name = $_FILES['photo']['name'];
+        $image_tmp = $_FILES['photo']['tmp_name'];
+        $path_photo = "./images/students_photo/";
+        move_uploaded_file($image_tmp, $path_photo . $image_name);
+
+        // Student ID Copy
+        $imageId_name = $_FILES['idcopy']['name'];
+        $imageId_tmp = $_FILES['idcopy']['tmp_name'];
+        $path_id_copy = "./images/students_id_copy/";
+        move_uploaded_file($imageId_tmp, $path_id_copy . $imageId_name);
+        // Send The Name To Student Attribute 
+        $studenClass->std_photo = $_FILES['photo']['name'];
+        $studenClass->std_id_copy = $_FILES['idcopy']['name'];
+    }
     $studenClass->insertStudent();
+    header("location:student.php");
+    exit();
+}
+if (isset($_POST['deleteStudent'])) {
+    $studenClass = new student_method();
+    $studenClass->std_id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+    $studenClass->deleteStudent($studenClass->std_id);
+    header("location:student.php");
+    exit();
+}
+if (isset($_POST['fetchById'])) {
+    $studenClass = new student_method();
+    $studenClass->std_id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+    $studenClass->fetchById($studenClass->std_id);
 }
 ?>
 <html>
@@ -26,20 +54,23 @@ if(isset($_POST['add'])){
         <title></title>
     </head>
     <body>
-        <form method="post">
-            <input type="text" name="name">
-            <input type="text" name="mobile">
-            <input type="text" name="email">
-            <input type="file" name="photo">
-            <input type="file" name="idcopy">
-            <input type="text" name="address">
-            <input type="text" name="age">
-            <input type="text" name="gender">
-            <input type="date" name="dob">
-            <input type="text" name="ref">
-            <input type="text" name="notes">
+        <form action="#" method="post" enctype="multipart/form-data">
+            <input type="text" name="id" placeholder="id test for delete">
+            <input type="text" name="name" placeholder="name">
+            <input type="text" name="mobile" placeholder="mobile">
+            <input type="text" name="email" placeholder="emial">
+            <input type="file" name="photo" pattern="photo">
+            <input type="file" name="idcopy" pattern="id photo">
+            <input type="text" name="address" placeholder="address">
+            <input type="text" name="age" placeholder="age">
+            <input type="text" name="gender" placeholder="geneder">
+            <input type="date" name="dob" placeholder="dob">
+            <input type="text" name="ref" placeholder="ref">
+            <input type="text" name="notes" placeholder="not">
             <button type="submit" name="add">Add</button>
-        <button type="submit" name="fetchAll">FetchAll</button>
+            <button type="submit" name="fetchAll">FetchAll</button>
+            <button type="submit" name="deleteStudent">Delete</button>
+            <button type="submit" name="fetchById">fetchById</button>
         </form>
     </body>
 </html>
